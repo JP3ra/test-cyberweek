d60e0730e633868dbd854dffe2dfa52799e391de
ompatil19 <ompatil2002@gmail.com>
2023-06-01 11:19:59 +0530

    created  triangle background

---
+++ src/components/landing/Landing.js
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react';
+import './Landing.css'
+// import './landingscript.js'
+export default function Landing() {
+    useEffect(() => {
+        const triangleBase = 48;
+
+const container = document.querySelector(".triangle-container");
+
+const instantiateGrid = () => {
+  container.innerHTML = '';
+  const width = document.body.clientWidth;
+  const heigth = document.body.clientHeight * 0.4;
+
+  let columns = Math.ceil(width / (triangleBase * 2)) + 1;
+  let rows = Math.ceil(heigth / triangleBase * 1.733);
+  container.style.setProperty('--columns', columns);
+
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < columns; x++) {
+      createTriangleSet(x + y * columns, x, y);
+    }
+  }
+}
+
+const createTriangleSet = (index, column, row) => {
+  let el = document.createElement("div");
+  el.classList.add("triangle-set");
+	if (row % 2 == 0) el.classList.add("triangle-set--offset");
+  
+  container.appendChild(el);
+}
+
+window.onresize = () => {
+  instantiateGrid();
+}
+
+instantiateGrid();
+const glow = document.body.querySelector("#glow")
+
+window.addEventListener('mousemove', (event) => {
+	glow.style.top = event.pageY + "px";
+	glow.style.left = event.pageX + "px";
+});
+      }, []);
+  return (
+<>
+<div cLass="hero">
+<div id="glow"></div>
+<div className="triangle-container"></div>
+<h1>CyberWeek</h1>
+</div>
+</>
+  )
+}
